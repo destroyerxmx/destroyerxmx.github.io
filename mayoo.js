d230d526dd87c6fd29f9fe2a240adda530a5bc87
destroyerxmx <134353830+destroyerxmx@users.noreply.github.com>
2023-05-22 23:48:55 -0400

    Update mayoo.js

--- mayoo.js
+++ mayoo.js
@@ -6,3 +6,13 @@ elements.mayoo = {
     state: "liquid",
     density: 720,
 };
+
+
+elements.soup = {
+  color: "#FFFF4D"
+  behavior: behaviors.LIQUID,
+  category: "liquids",
+  viscocity: 74000,
+  state: "liquid",
+  density: 720
+};
